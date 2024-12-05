ef3f16720b335796c801752668ccb6c83e89aa13
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-12-05 14:47:04 +0100

    Update TextDecorator.java

--- src/main/java/org/example/TextDecorator.java
+++ src/main/java/org/example/TextDecorator.java
@@ -3,7 +3,12 @@ package org.example;
 public abstract class TextDecorator implements IText {
     protected final IText text;
 
-    protected TextDecorator(IText text) {
+    public TextDecorator(IText text) {
         this.text = text;
+    }
+
+    @Override
+    public String transform(){
+        return text.transform();
     }
 }
